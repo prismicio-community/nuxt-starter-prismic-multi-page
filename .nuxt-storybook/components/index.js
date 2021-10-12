cc8c48df11e84c22f6361f8ce4e153c885462224
lihbr <lihbr@users.noreply.github.com>
2021-10-12 12:09:01 +0200

    chore: don't version .nuxt-storybook

--- .nuxt-storybook/components/index.js
+++
@@ -1,7 +0,0 @@
-import { wrapFunctional } from './utils'
-
-export { default as HeaderPrismic } from '../../components/HeaderPrismic.vue'
-export { default as Logo } from '../../components/Logo.vue'
-
-export const LazyHeaderPrismic = import('../../components/HeaderPrismic.vue' /* webpackChunkName: "components/header-prismic" */).then(c => wrapFunctional(c.default || c))
-export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
