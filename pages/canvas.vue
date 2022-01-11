47fd281867f0f133d4967fce930d0d0b509289b8
Phil Snow <phil.snow@prismic.io>
2022-01-11 16:12:31 +0100

    adding slicecanvas

---
+++ pages/canvas.vue
@@ -0,0 +1,22 @@
+<template>
+	<SliceCanvasRenderer :state="state" #default="props">
+		<SliceZone v-bind="props" />
+	</SliceCanvasRenderer>
+</template>
+
+<script>
+import { SliceCanvasRenderer } from "@prismicio/slice-canvas-renderer-vue";
+import SliceZone from "vue-slicezone";
+
+import state from "~/.slicemachine/libraries-state.json";
+
+export default {
+	components: {
+		SliceCanvasRenderer,
+		SliceZone
+	},
+	data() {
+		return { state };
+	}
+}
+</script>
