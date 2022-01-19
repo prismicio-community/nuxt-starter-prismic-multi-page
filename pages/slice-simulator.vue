37e69359b1f92eb83637bbc758a5b1bbd19e60a0
Phil Snow <phil.snow@prismic.io>
2022-01-19 16:09:28 +0100

    Slice Simulator update

---
+++ pages/slice-simulator.vue
@@ -0,0 +1,22 @@
+<template>
+  <SliceSimulator :state="state" #default="{ slices }">
+    <SliceZone :slices="slices" />
+  </SliceSimulator>
+</template>
+
+<script>
+import { SliceSimulator } from "@prismicio/slice-simulator-vue";
+import SliceZone from "vue-slicezone";
+
+import state from "~~/.slicemachine/libraries-state.json";
+
+export default {
+  components: {
+    SliceSimulator,
+    SliceZone
+  },
+  data() {
+    return { state };
+  }
+}
+</script>
