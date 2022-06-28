97db812ede451200a661b395b51339c06d00ebcf
Sam Littlefair <31219208+samlfair@users.noreply.github.com>
2022-06-28 18:57:37 -0300

    docs: add documentation and demo documents (#10)
    * chore: Add demo documents
    
    * docs: Add docs/readme, update main readme, and documentation banner
    
    * docs: update documentation banner content
    
    * chore: Remove vscode settings
    
    * chore: ignore vscode settings

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="text-slate-800">
+    <!-- TODO: Remove the following element once you have read the documentation. -->
+    <div
+      v-if="$config.development"
+      :style="{
+        background: '#5163ba',
+        padding: '1rem',
+        textAlign: 'center',
+        fontSize: '0.85rem',
+        color: '#fff'
+      }"
+    >
+      <p>
+        <strong>👋 Welcome to your new website!</strong> To customize the code
+        and content of this site,&nbsp;<a
+          href="https://github.com/prismicio-community/nuxt-starter-prismic-multi-page/tree/master/docs"
+          target="_blank"
+          rel="noreferrer"
+          :style="{
+            textDecoration: 'underline'
+          }"
+          >see the documentation</a
+        >. Remove this bar in <code>layouts/default.vue</code>.
+      </p>
+    </div>
     <Header
       v-if="navigation.data && settings.data"
       :navigation="navigation"
@@ -12,10 +36,10 @@
 <script>
 export default {
   computed: {
-    navigation () {
+    navigation() {
       return this.$store.state.prismic.navigation
     },
-    settings () {
+    settings() {
       return this.$store.state.prismic.settings
     }
   }
